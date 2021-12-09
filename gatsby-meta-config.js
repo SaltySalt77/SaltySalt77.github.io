63e3c47d067bab524e641da090f67e0828847413
hyna <alexisna@naver.com>
2021-12-10 03:40:32 +0900

    ch meta file

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  title: `Salt House`,
+  description: `hey, it's too salty!`,
+  author: `hyna`,
+  introduction: `This is the engineering blog for developer hyna`,
   siteUrl: `https://saltysalt77.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `SaltySalt77`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
