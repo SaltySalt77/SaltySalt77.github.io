18d0007e9566ea7be61d63aed8d6980d52829f65
hyna <alexisna@naver.com>
2021-12-15 22:42:19 +0900

    changed meta config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -16,7 +16,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'SaltySalt77/github.io_comments.git', // Your repository for archive comment
+    utterances: 'saltysalt77/blog_comments.git', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
